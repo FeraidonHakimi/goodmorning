9abfaff7c96101381abb8c26aa70c4a92f04b89e
feraidonHakimi <feraidon.hakimi@tekschool.us>
2022-11-09 11:03:13 -0500

    this is second class

---
+++ src/breakFast/second.java
@@ -0,0 +1,11 @@
+package breakFast;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("faizi koja asty sadayet beron namosha");
+
+	}
+
+}
